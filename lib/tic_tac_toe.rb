174bf005f816511c18355508e1b97154dfe35c46
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-12 21:30:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,14 +63,6 @@ def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
-def play(board)
-  turn_count = 0
-  while turn_count < 9
-    turn(board)
-    turn_count+=1
-  end
-end
-
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
@@ -94,5 +86,13 @@ end
 def winner(board)
   if winning_combo = won?(board)
     board[winning_combo.first]
+  end
+end
+
+def play(board)
+  turn_count = 0
+  while turn_count < 9
+    turn(board)
+    turn_count+=1
   end
 end
